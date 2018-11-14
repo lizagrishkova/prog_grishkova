4d14cfa10ea6dc0506f7b0e8874c96f21a83906a
lizagrishkova <grishkova.ea@phystech.edu>
2018-11-14 23:06:46 +0300

    Нормально нарисовала пушку. Начала разбирать код с семинара. Проблемы с полетом по параболе

--- cannon.py
+++ cannon.py
@@ -1,33 +1,27 @@
 import math
 from tkinter import *
 
-root = Tk()
-root.geometry('800x600')        
-canvas = Canvas(root, bg='white') 
-canvas.pack(fill=BOTH, expand=1)
 
 g = 9.8  # Ускорение свободного падения для снаряда.
-r = 20  # Радиус пушечного ядра
-
-
-def draw_cannon():
-    cannon = canvas.create_oval(0, 500, 100, 600, fill='black')
-    cannon_muzzle = canvas.create_line(50, 550, 100, 500, fill='black', width=30)
 
 
 class Cannon:
-    max_velocity = 10
+    max_velocity = 1000
 
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+    def __init__(self, canvas):
+        self.power = 10
+        self.x = 50
+        self.y = 550
+        self.r = 50
+        self.canvas = canvas
         self.shell_num = None  # TODO: оставшееся на данный момент количество снарядов
         self.direction = math.pi / 4
+        self.line_length = 100
+        self.line = canv.create_line(self.x, self.y, self.x + self.line_length, self.y, width=30, fill="black")
+        self.oval = canv.create_oval(self.x - self.r, self.y + self.r, self.x + self.r, self.y - self.r,
+                                     outline="black", fill="black")
 
-    def aim(self, event):
-        x = event.x
-        y = event.y
-        self.direction = math.atan((y - self.y)/(x - self.x))
+    def aim(self, x, y):
         """
         Меняет направление direction так, чтобы он из точки
          (self.x, self.y) указывал в точку (x, y).
@@ -35,10 +29,10 @@ class Cannon:
         :param y: координата y, в которую целимся
         :return: None
         """
+        self.direction = math.atan((self.y - y)/(self.x - x))
+        self.draw()
 
     def fire(self, dt):
-        if self.shell_num > 0:
-            cannonball = canvas.create_oval(self.x-r, self.y-r, self.x+r, self.y+r, fill=random.choice(colors))
         """
         Создаёт объект снаряда (если ещё не потрачены все снаряды)
         летящий в направлении угла direction
@@ -46,31 +40,53 @@ class Cannon:
         :param dt:  длительность клика мышки, мс
         :return: экземпляр снаряда типа Shell
         """
-        pass
+
+        shell = Shell(self.x + self.line_length*math.cos(self.direction),
+                      self.y + self.line_length*math.sin(self.direction), self.canvas)
+
+        shells.append(shell)
+
+    def draw(self):
+
+        self.canvas.delete(self.line)
+
+        self.line = self.canvas.create_line(
+            self.x,
+            self.y,
+            self.x + self.line_length*math.cos(self.direction),
+            self.y + self.line_length*math.sin(self.direction), width=30, fill="black"
+        )
 
 
 class Shell:
-    standard_radius = 5
+    global standard_radius
+    standard_radius = 30
 
-    def __init__(self, x, y, vx, vy):
+    def __init__(self, x, y, canvas):
         self.x, self.y = x, y
-        self.vx, self.vy = vx, vy
-        self.r = standard_raduis
+        self.Vx, self.Vy = 500, -500
+        self.dx, self.dy = 0, 0
+        self.r = standard_radius
+        self.canvas = canvas
+        self.oval = self.canvas.create_oval(self.x - self.r, self.y - self.r,
+                                            self.x + self.r, self.y + self.r, fill='cyan')
 
     def go(self, dt):
-        """
-        Сдвигает снаряд исходя из его кинематических характеристик
-        и длины кванта времени dt
-        в новое положение, а также меняет его скорость.
-        :param dt:
-        :return:
-        """
         ax, ay = 0, g
-        self.x += self.vx*dt + ax*(dt**2)/2
-        self.y += self.vy*dt + ay*(dt**2)/2
-        self.vx += ax*dt
-        self.vy += ay*dt
+        self.dx = self.Vx * dt + ax * (dt ** 2) / 2
+        self.dy = self.Vy * dt + ay * (dt ** 2) / 2
+        self.x += self.dx
+        self.y += self.dy
+        self.Vx += ax * dt
+        self.Vy -= ay * dt
+
+        print('x = {}, y = {}'.format(self.x, self.y))
+        self.draw()
+
         # TODO: Уничтожать (в статус deleted) снаряд, когда он касается земли.
+
+    def draw(self):
+        self.canvas.move(self.oval, self.dx, self.dy)
 
     def detect_collision(self, other):
         """
@@ -78,17 +94,18 @@ class Shell:
         :param other: объект, который должен иметь поля x, y, r
         :return: логическое значение типа bool
         """
-        length = ((self.x - other.x)**2 + (self.y - other.y)**2)**0.5
+
+        length = ((self.x - other.x) ** 2 + (self.y - other.y) ** 2) ** 0.5
         return length <= self.r + other.r
 
 
 class Target:
     standard_radius = 5
 
-    def __init__(self, x, y, vx, vy):
+    def __init__(self, x, y, Vx, Vy):
         self.x, self.y = x, y
-        self.vx, self.vy = vx, vy
-        self.r = standard_raduis
+        self.Vx, self.Vy = Vx, Vy
+        self.r = standard_radius
 
     def go(self, dt):
         """
@@ -98,11 +115,13 @@ class Target:
         :param dt:
         :return:
         """
+
         ax, ay = 0, g
-        self.x += self.vx * dt
-        self.y += self.vy * dt
-        self.vx += ax * dt
-        self.vy += ay * dt
+        self.x += self.Vx * dt + ax * (dt ** 2) / 2
+        self.y += self.Vy * dt - ay * (dt ** 2) / 2
+        self.Vx += ax * dt
+        self.Vy -= ay * dt
+
         # TODO: Шарики-мишени должны отражаться от стенок
 
     def collide(self, other):
@@ -111,9 +130,37 @@ class Target:
         :param other:
         :return:
         """
+
         pass  # TODO
 
 
-cannon = Cannon(40, 40)
-draw_cannon()
-canvas.bind('<Motion>', cannon.aim)
+def mouse_move_handler(event):
+    cannon.aim(event.x, event.y)
+
+
+def mouse_left_click_handler(event):
+    cannon.fire(100)
+
+
+def tick():
+    for shell in shells:
+        shell.go(0.01)
+
+    root.after(100, tick)
+
+
+root = Tk()
+fr = Frame(root)
+root.geometry('800x600')
+canv = Canvas(root, bg='white')
+canv.pack(fill=BOTH, expand=1)
+
+canv.bind('<Motion>', mouse_move_handler)
+canv.bind('<Button-1>', mouse_left_click_handler)
+
+shells = []
+
+cannon = Cannon(canv)
+
+tick()
+root.mainloop()
